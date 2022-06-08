cfd62975b7366f2d24e2715e743bc9b168080d7d
Michael Amrhein <michael@adrhinum.de>
2022-06-08 23:41:59 +0200

    Added handling of multiple tabs.

--- cocomo-tui/src/app.rs
+++ cocomo-tui/src/app.rs
@@ -7,7 +7,7 @@
 // $Source$
 // $Revision$
 
-use std::{io, ops::Add};
+use std::{borrow::BorrowMut, io, ops::Add};
 
 use crossterm::{
     event,
@@ -22,7 +22,7 @@ use tui::{
     Frame, Terminal,
 };
 
-use crate::session::Session;
+use crate::{session::Session, view::View};
 
 pub(crate) struct App {
     sessions: Vec<Session>,
@@ -47,6 +47,11 @@ impl App {
         &self.sessions[self.curr_session_idx]
     }
 
+    #[inline(always)]
+    pub(crate) fn curr_session_mut(&mut self) -> &mut Session {
+        &mut self.sessions[self.curr_session_idx]
+    }
+
     pub(crate) fn next_session(&mut self) -> bool {
         if self.n_sessions() == 1 {
             return false;
@@ -69,6 +74,7 @@ impl App {
     pub(crate) fn add_session(&mut self) {
         // TODO: call new session params popup
         let session = Session::new(
+            self.n_sessions() + 1,
             Some("fake".to_string()),
             self.curr_session().left.clone(),
             self.curr_session().right.clone(),
@@ -84,10 +90,7 @@ impl App {
     ) -> io::Result<()> {
         let mut redraw = true;
         loop {
-            if redraw {
-                terminal.draw(|f| self.draw(f))?;
-            }
-            redraw = false;
+            terminal.draw(|f| self.draw(f))?;
             if let Event::Key(key) = event::read()? {
                 match key.code {
                     KeyCode::Char('q') => break,
@@ -121,7 +124,7 @@ impl App {
         Ok(())
     }
 
-    pub(crate) fn draw<B: Backend>(&self, frame: &mut Frame<B>) {
+    pub(crate) fn draw<B: Backend>(&mut self, frame: &mut Frame<B>) {
         let size = frame.size();
         let chunks = Layout::default()
             .direction(Direction::Vertical)
@@ -134,6 +137,7 @@ impl App {
                 .as_ref(),
             )
             .split(size);
+        // TODO: replace by TabBar
         let titles = self
             .sessions
             .iter()
@@ -148,12 +152,10 @@ impl App {
             .highlight_style(Style::default().bg(Color::Gray))
             .divider("|");
         frame.render_widget(tabs, chunks[0]);
-        frame.render_widget(
-            Block::default()
-                .title(format!("view '{}'", self.curr_session_idx))
-                .borders(Borders::ALL),
-            chunks[1],
-        );
+        // Session view
+        let session = self.curr_session_mut();
+        session.set_area(chunks[1]).draw::<B>(frame);
+        // TODO: replace by CmdBar
         let cmd_bar = Paragraph::new(Spans::from(vec![
             Span::styled("Quit [q]", Style::default().bg(Color::LightYellow)),
             Span::raw(" "),
--- cocomo-tui/src/session.rs
+++ cocomo-tui/src/session.rs
@@ -10,8 +10,18 @@
 use std::rc::Rc;
 
 use cocomo_core::{FSItem, ItemType};
+use tui::{
+    backend::Backend,
+    layout::{Constraint, Rect},
+    widgets::{Block, Borders},
+    Frame,
+};
+
+use crate::view::View;
 
 pub(crate) struct Session {
+    id: usize,
+    area: Rect,
     pub(crate) name: String,
     pub(crate) left: Rc<FSItem>,
     pub(crate) right: Rc<FSItem>,
@@ -19,12 +29,15 @@ pub(crate) struct Session {
 
 impl Session {
     pub(crate) fn new(
+        id: usize,
         name: Option<String>,
         left: Rc<FSItem>,
         right: Rc<FSItem>,
     ) -> Self {
         assert_eq!(left.item_type, right.item_type);
         Self {
+            id,
+            area: Rect::default(),
             name: name.unwrap_or("".to_string()),
             left,
             right,
@@ -33,5 +46,29 @@ impl Session {
 
     pub(crate) fn session_type(&self) -> ItemType {
         self.left.item_type
+    }
+}
+
+impl View for Session {
+    fn area(&self) -> Rect {
+        self.area
+    }
+
+    fn set_area(&mut self, area: Rect) -> &Self {
+        self.area = area;
+        self
+    }
+
+    fn want_layout(&self) -> Constraint {
+        Constraint::Min(3)
+    }
+
+    fn draw<B: Backend>(&self, frame: &mut Frame<B>) {
+        frame.render_widget(
+            Block::default()
+                .title(format!("view '{}'", self.id))
+                .borders(Borders::ALL),
+            self.area,
+        );
     }
 }
