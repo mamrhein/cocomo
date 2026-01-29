54e9f08ab1ed6c13e65838511175822668198ddf
Michael Amrhein <michael@adrhinum.de>
2026-01-29 22:36:24 +0100

    Added inferring the file type.

--- cocomo-core/src/fsitem.rs
+++ cocomo-core/src/fsitem.rs
@@ -9,9 +9,17 @@
 
 use std::{fmt, fs, io, path};
 
-// TODO: replace by struct from extern file type matcher (maybe 'infer').
-#[derive(Copy, Clone, Debug, PartialEq)]
-pub struct FileType {}
+use infer::get_from_path;
+
+#[derive(Clone, Debug, PartialEq, Default)]
+pub enum FileType {
+    #[default]
+    Unknown,
+    Inferred {
+        mime: &'static str,
+        ext: &'static str,
+    },
+}
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum FSItemType {
@@ -43,15 +51,21 @@ pub struct FSItem {
 }
 
 impl FSItem {
-    fn new(path: &path::PathBuf, meta: &fs::Metadata) -> io::Result<Self> {
+    fn new(path: &path::Path, meta: &fs::Metadata) -> io::Result<Self> {
         let path = path.canonicalize()?;
-        // TODO: examine file type
-        let file_type = FileType {};
         Ok(Self {
             item_type: match meta {
                 m if m.is_dir() => FSItemType::Directory,
-                m if m.is_file() => FSItemType::File {
-                    file_type: file_type,
+                m if m.is_file() => match get_from_path(&path) {
+                    Ok(Some(guess)) => FSItemType::File {
+                        file_type: FileType::Inferred {
+                            mime: guess.mime_type(),
+                            ext: guess.extension(),
+                        },
+                    },
+                    _ => FSItemType::File {
+                        file_type: FileType::default(),
+                    },
                 },
                 m if m.is_symlink() => FSItemType::SymLink {
                     path: fs::read_link(&path)?,
@@ -60,7 +74,7 @@ impl FSItem {
                     return Err(io::Error::new(
                         io::ErrorKind::Unsupported,
                         "Unknown directory entry",
-                    ))
+                    ));
                 }
             },
             name: path.file_name().unwrap().to_string_lossy().into(),
@@ -88,12 +102,27 @@ impl FSItem {
     pub fn metadata(&self) -> &fs::Metadata {
         &self.metadata
     }
+
+    #[inline(always)]
+    pub fn is_dir(&self) -> bool {
+        self.item_type == FSItemType::Directory
+    }
+
+    #[inline(always)]
+    pub fn is_file(&self) -> bool {
+        matches!(self.item_type, FSItemType::File { .. })
+    }
+
+    #[inline(always)]
+    pub fn is_link(&self) -> bool {
+        matches!(self.item_type, FSItemType::SymLink { .. })
+    }
 }
 
-impl TryFrom<&String> for FSItem {
+impl TryFrom<&str> for FSItem {
     type Error = io::Error;
 
-    fn try_from(s: &String) -> Result<Self, Self::Error> {
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
         let path = path::PathBuf::from(s);
         let meta = fs::metadata(&path)?;
         Self::new(&path, &meta)
@@ -105,5 +134,24 @@ impl TryFrom<&fs::DirEntry> for FSItem {
 
     fn try_from(item: &fs::DirEntry) -> Result<Self, Self::Error> {
         Self::new(&item.path(), &item.metadata()?)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dir() {
+        let dir = FSItem::try_from(".").unwrap();
+        assert!(dir.is_dir());
+        assert_eq!(dir.name(), "cocomo-core");
+    }
+
+    #[test]
+    fn test_file() {
+        let file = FSItem::try_from("./Cargo.toml").unwrap();
+        assert!(file.is_file());
+        assert_eq!(file.name(), "Cargo.toml");
     }
 }
