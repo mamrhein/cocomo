f848935107be9b2682fa91dd3a0fa2ff51bacd74
Michael Amrhein <michael@adrhinum.de>
2026-07-07 15:36:54 +0200

    Replaced meaningless "Hello world!" stubs

--- cocomo-gui/src/main.rs
+++ cocomo-gui/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+// ---------------------------------------------------------------------------
+// Copyright:   (c) 2026 ff. Michael Amrhein (michael@adrhinum.de)
+// License:     This program is part of a larger application. For license
+//              details please read the file LICENSE.TXT provided together
+//              with the application.
+// ---------------------------------------------------------------------------
+// $Source$
+// $Revision$
+
+use anyhow::Result;
+
+#[allow(unreachable_code)]
+#[tokio::main]
+async fn main() -> Result<()> {
+    !unimplemented!()
 }
